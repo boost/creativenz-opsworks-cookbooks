f18f06bfcab54d2c3501717a597d5587f93dbc47
Klaus Hott Vidal <klahott@gmail.com>
2016-10-12 09:39:32 +1300

    Forcing 2.3

--- ruby/attributes/customize.rb
+++ ruby/attributes/customize.rb
@@ -5,3 +5,10 @@
 # "ruby/attributes/customize.rb" in your cookbook repository and
 # put the overrides in YOUR customize.rb file.
 ###
+default[:ruby][:major_version] = "2"
+default[:ruby][:minor_version] = "3"
+default[:ruby][:patch_version] = "1"
+default[:ruby][:pkgrelease]    = "1"
+
+default[:ruby][:full_version] = [node[:ruby][:major_version], node[:ruby][:minor_version]].join(".")
+default[:ruby][:version] = [node[:ruby][:full_version], node[:ruby][:patch_version]].join(".")
